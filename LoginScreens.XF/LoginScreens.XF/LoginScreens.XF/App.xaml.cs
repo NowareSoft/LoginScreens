0376973b6f49bc66b8a83a3e76dc9a51a90672f2
Noware Software <Noware19@hotmail.com>
2019-10-18 03:03:26 -0300

    Login One Finish

---
+++ LoginScreens.XF/LoginScreens.XF/LoginScreens.XF/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using LoginScreens.XF.Views;
+using Xamarin.Forms;
+
+namespace LoginScreens.XF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginOnePage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ LoginScreens.XF/LoginScreens.XF/LoginScreens.XF/Implementations/MyEntry.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace LoginScreens.XF.Implementations
+{
+    public class MyEntry : Entry
+    {
+
+    }
+}
